26225b9934cc166226e966d22478963b24a9a2f9
Bashar Eid <bashar.eid@viam.com>
2025-12-05 13:51:04 -0500

    add types to variables

--- ci/variables.tf
+++ ci/variables.tf
@@ -1,32 +1,39 @@
 variable "tenancy_ocid" {
+  type = string
   description = "Tenancy's OCID"
 }
 
 variable "user_ocid" {
+  type = string
   description = "User's OCID"
   default = ""
 }
 
 variable "fingerprint" {
+  type = string
   description = "Key Fingerprint"
   default     = ""
 }
 
 variable "private_key_path" {
+  type = string
   description = "The private key path to pem."
   default     = ""
 }
 
 variable "region" {
+  type = string
   description = "OCI Region"
 }
 
 variable "ssh_authorized_keys_path" {
+  type = string
   description = "Public SSH keys path to be included in the ~/.ssh/authorized_keys file for the default user on the instance."
   default     = ""
 }
 
 variable "ssh_private_key_path" {
+  type = string
   description = "The private key path to access instance."
   default     = ""
 }
